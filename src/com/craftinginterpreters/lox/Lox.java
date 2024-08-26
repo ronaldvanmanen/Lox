51ee7ed345dc126eaa6828f16f3a80eb5328acc3
Ronald van Manen <rvanmanen@gmail.com>
2024-08-26 12:57:26 +0200

    We’ll use this to ensure we don’t try to execute code that has a known error. Also, it lets us exit with a non-zero exit code like a good command line citizen should.

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class Lox {
   static boolean hadError = false;
-  
+
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
@@ -25,6 +25,8 @@ public class Lox {
   private static void runFile(String path) throws IOException {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
+    // Indicate an error in the exit code.
+    if (hadError) System.exit(65);    
   }
 
   private static void runPrompt() throws IOException {
