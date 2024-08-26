5fe2365d9d7c085d04e9f4660c3476058b764ab1
Ronald van Manen <rvanmanen@gmail.com>
2024-08-26 14:41:31 +0200

    Rename match to advanceIf because it’s like a conditional advance().

--- src/com/craftinginterpreters/lox/Scanner.java
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -47,16 +47,16 @@ class Scanner {
       case ';': addToken(SEMICOLON); break;
       case '*': addToken(STAR); break;
       case '!':
-        addToken(match('=') ? BANG_EQUAL : BANG);
+        addToken(advanceIf('=') ? BANG_EQUAL : BANG);
         break;
       case '=':
-        addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+        addToken(advanceIf('=') ? EQUAL_EQUAL : EQUAL);
         break;
       case '<':
-        addToken(match('=') ? LESS_EQUAL : LESS);
+        addToken(advanceIf('=') ? LESS_EQUAL : LESS);
         break;
       case '>':
-        addToken(match('=') ? GREATER_EQUAL : GREATER);
+        addToken(advanceIf('=') ? GREATER_EQUAL : GREATER);
         break;
       default:
         Lox.error(line, "Unexpected character.");
@@ -68,7 +68,7 @@ class Scanner {
     return source.charAt(current++);
   }
 
-  private boolean match(char expected) {
+  private boolean advanceIf(char expected) {
     if (isAtEnd()) return false;
     if (source.charAt(current) != expected) return false;
     current++;
